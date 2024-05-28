50b732ec47d8936c4d2fe944dd14886d1677856b
LeThiHongNgoc <ngocb2005766@student.ctu.edu.vn>
2024-05-28 13:58:41 +0700

    ProgressPage

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import HomePage from "../pages/HomePage/HomePage";
 import NotFoundPage from "../pages/NotFound/NotFoundPage";
+import ProgressPage from "../pages/ProgressPage/ProgressPage";
 
 const routes = [
   {
     path: "/",
     name: "HomePage",
     page: HomePage,
+  },
+  {
+    path: "/progress",
+    name: "ProgressPage",
+    page: ProgressPage,
   },
   {
     path: "*",
